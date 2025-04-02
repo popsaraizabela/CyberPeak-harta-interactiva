8a34d895398d09d56507e1e3f089e291c40d4f01
popsaraizabela <popsaraizabela@gmail.com>
2025-04-02 16:06:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 let renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-
-window.onload = () => {
-    document.getElementById("container").appendChild(renderer.domElement);
-};
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
 
 let geometry = new THREE.SphereGeometry(1, 32, 32);
 let texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/8/80/Earth_Western_Hemisphere_transparent_background.png');
@@ -19,11 +17,13 @@ function animate() {
     sphere.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
-animate();
 
-setTimeout(() => {
-    document.getElementById("container").style.display = "none";
-    document.getElementById("map").style.display = "block";
-    let map = L.map('map').setView([45, 25], 3);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-}, 5000);
+window.onload = () => {
+    animate();
+    setTimeout(() => {
+        document.getElementById("container").style.display = "none";
+        document.getElementById("map").style.display = "block";
+        let map = L.map('map').setView([45, 25], 3);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    }, 5000);
+};
