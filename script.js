596006e96e1981191136140f812c27705d1d93a6
popsaraizabela <popsaraizabela@gmail.com>
2025-04-02 17:02:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
 
-// Inițializare hartă
-var map = L.map('map').setView([45.9432, 24.9668], 6); // Centrat pe România
-
-// Adăugare strat de hartă OpenStreetMap
+const map = L.map('map').setView([45.9432, 24.9668], 6);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Array pentru a stoca markerele
-var markers = [];
-
-// Funcție pentru a adăuga un marker la click
-function onMapClick(e) {
-    var marker = L.marker(e.latlng).addTo(map)
-        .bindPopup("Loc marcat:<br>" + e.latlng.toString())
-        .openPopup();
+let markers = [];
+map.on('click', function (e) {
+    const marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
     markers.push(marker);
-}
+});
 
-// Funcție pentru a șterge toate markerele
 function clearMarkers() {
     markers.forEach(marker => map.removeLayer(marker));
     markers = [];
 }
-
-// Ascultă evenimentul de click pe hartă
-map.on('click', onMapClick);
