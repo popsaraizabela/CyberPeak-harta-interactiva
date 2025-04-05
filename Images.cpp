f654c9ea678888a75a77dc16b7fe8829b4255988
CiorbaDragos <dragosmihaiciorba@gmail.com>
2025-04-05 09:09:15 +0300

    Add files via upload

---
+++ Images.cpp
@@ -0,0 +1,16 @@
+////images
+//#include<iostream>
+//#include<opencv2/imgcodecs.hpp>
+//#include<opencv2/highgui.hpp>
+//#include<opencv2/imgproc.hpp>
+//using namespace std;
+//using namespace cv;
+//int main()
+//{
+//	string path ="C:/Users/HP ULTRABOOK/OneDrive/Desktop/CyberPeak/Cyberpeak/Resources/test.png";
+//		Mat image = imread(path);
+//		imshow("Image", image);
+//		waitKey(0);
+//   return 0;
+//}
+
